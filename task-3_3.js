20908ff26e568e95c11db4553e886810607b3d61
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2023-07-25 17:44:46 +0300

    add code task-3_3

--- task-3_3.js
+++ task-3_3.js
@@ -1,3 +1,15 @@
 // Напишіть скрипт, який перевіряє наявність в масиві 
 // ["Pyton", "C++", "C#", "PHP"] значення "Java Script".
-// Якщо в масиві такого значення немає вивести в консоль "Значення Java Script в списку немає, додаємо в масив". Після чого додати "Java Script" в кінець масиву.
+// Якщо в масиві такого значення немає вивести в консоль 
+// "Значення Java Script в списку немає, додаємо в масив". 
+// Після чого додати "Java Script" в кінець масиву.
+
+const items = ["Pyton", "C++", "C#", "PHP"]
+let itemPush = "Java Script"
+let itemSearch = items.includes(itemPush)
+
+    if(itemSearch === false){
+        console.log(`Значення Java Script в списку немає, додаємо в масив`)
+        items.push(itemPush)
+    }      
+console.log(items)
